4c2472fecadfff552c4c3fa3576a1b8af505766d
icelz <47770748+icelz@users.noreply.github.com>
2024-02-03 15:59:52 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+
 /*
 miniProxy - A simple PHP web proxy. <https://github.com/joshdick/miniProxy>
 Written and maintained by Joshua Dick <http://joshdick.net>.
